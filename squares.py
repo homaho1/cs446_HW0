5e53d411af099e09b2aad8c3c8ca5cbca47d7e9b
Ho-Chih Ma <78931864+homaho1@users.noreply.github.com>
2023-08-29 01:46:55 -0500

    Update squares.py

--- squares.py
+++ squares.py
@@ -4,7 +4,7 @@ import torch
 def numpy_squares(k):
   """return (1, 4, 9, .., k'2) as a numpy array"""
   # your code here
-  arr = numpy.arange([x**2 for x in range(1, k+1, 1)])
+  arr = numpy.arange(1, k+1, 1)**2
   return arr
 
 def torch_squares(k):
